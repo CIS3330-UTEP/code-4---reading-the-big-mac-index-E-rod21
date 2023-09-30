ac721db548103d01f12e9d42166cfe9f467038f8
E-rod21 <rodriguez.esau19@outlook.com>
2023-09-29 22:07:09 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -2,17 +2,66 @@ import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
 
+df = pd.read_csv(big_mac_file)
+
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+
+    year = year
+    country_code = country_code.upper()
+    
+    query = f"(date >= '{year}-01-01') and (date <= '{year}-12-31') and (iso_a3 == '{country_code}')"
+
+    df_result = df.query(query)
+
+    mean_dollar_price = round(df_result['dollar_price'].mean(),2)
+
+    return mean_dollar_price
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    
+    country_code = country_code.upper()
+    
+    query = f"(iso_a3 == '{country_code}')"
+
+    df_result = df.query(query)
+
+    mean_dollar_price = round(df_result['dollar_price'].mean(),2)
+
+    return mean_dollar_price
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+
+    year = year
+    
+    query = f"(date >= '{year}-01-01') and (date <= '{year}-12-31')"
+    
+    df_result = df.query(query)
+
+    min_dollar_price =df_result['dollar_price'].idxmin()
+
+    row = df_result.loc[min_dollar_price]
+    result = f"{row['name']}({row['iso_a3']}): ${round(row['dollar_price'],2)}"
+    
+    return result
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    
+    year = year
+    
+    query = f"(date >= '{year}-01-01') and (date <= '{year}-12-31')"
+    
+    df_result = df.query(query)
+
+    max_dollar_price = df_result['dollar_price'].idxmax()
+    
+    row = df_result.loc[max_dollar_price]
+    
+    result = f"{row['name']}({row['iso_a3']}): ${round(row['dollar_price'],2)}"
+
+    return result
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    print(get_big_mac_price_by_year(input("Enter a Year: "),input("Enter your country code: ")))
+    print(get_big_mac_price_by_country(input("Enter a country code: ")))
+    print(get_the_cheapest_big_mac_price_by_year(2008))
+    print(get_the_most_expensive_big_mac_price_by_year(2003))
